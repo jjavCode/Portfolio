4a9d3dcfc67d656a0617782798ba6a2f805a253b
jjavCOde <jhordammv@gmail.com>
2023-07-28 06:10:38 -0400

    V2

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,9 @@ const desplazarArriba = document.querySelector("#desplazarse-hacia-arriba");
 
 desplazarArriba.addEventListener("click", () => {
     window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
+        top: 0,
+        left: 0,
+        behavior: "smooth",
     });
 });
 
